c7fd7ee1e679ff426e6c6b7d6212b6bdc056dd04
josemanuelgalvanhernandez <jose.galvan.oracle@gmail.com>
2026-03-12 16:11:52 -0600

    +Clase SAST+

--- src/main/java/VulnerableApp.java
+++ src/main/java/VulnerableApp.java
@@ -6,16 +6,10 @@ public class VulnerableApp {
 
     public static void main(String[] args) throws Exception {
 
-        String input = args.length > 0 ? args[0] : "test";
+        String cmd = args.length > 0 ? args[0] : "test";
 
-        // Command Injection
-        Runtime.getRuntime().exec("ls " + input);
+        // Vulnerabilidad: Command Injection
+        Runtime.getRuntime().exec(cmd);
 
-        // Path Traversal
-        File file = new File("data/" + input);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-
-        br.readLine();
-        br.close();
     }
 }
